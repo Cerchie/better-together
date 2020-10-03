b490f5cc16ddb1e48fe6b9f0c7e8ebd061905f75
Jake <jakefrisbee@gmail.com>
2020-10-03 10:04:53 -0600

    added scenarios.py

---
+++ scenarios.py
@@ -0,0 +1,7 @@
+scenario1 = {
+    "id": 1,
+    "title": "",
+    "description":
+    "suggestions": ["Tell me more about ___", "Have you considered ____", "We don't agree on ___ but we can agree on _____"]
+    "insights": ["How am I processing the experience?", "What body sensations do I have?", "What is my emotional reaction?"]
+}
