8a80098d3729de4e9c45d136a3e7fddedfb80492
LSanchez17 <galaxyplayeremail@gmail.com>
2020-10-03 15:19:57 -0400

    all files maybe

---
+++ forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired
+
+class EnterScenario(FlaskForm):
+    """Add a scenario to list"""
+
+    title = StringField('Title', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    suggestions = StringField('Suggestions(Separate by comma)', validators=[DataRequired()])
+    insights = StringField('Insights(Separate by comma', validators=[DataRequired()])
+
