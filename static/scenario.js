f80a196e58c64ba6f88f8d7539ebeaa0657e5c8f
Lucia Cerchie <luciacerchie@gmail.com>
2020-10-03 11:02:29 -0700

    moved scenario.js to static and linked in index

--- static/scenario.js
+++ static/scenario.js
@@ -5,14 +5,16 @@ const form = document.querySelector('#form-user-response');
 const input = document.querySelector('#form-input');
 
 btn.addEventListener('click', function (e) {
+    console.log('click')
     e.preventDefault();
-    answer.removeClass('.hidden')
+    answer.removeClass('.hidden');
 });
 
 input = document.getElementById("userInput").value;
 
 form.addEventListener('submit', function (e) {
+    console.log('click')
     e.preventDefault();
-    input.val
+    responseDiv.innerText = input.value;
 
-})
+});
