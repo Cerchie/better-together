b9d84ecbc68977a89833ec7716c5c0e4fb2211e0
Jake <jakefrisbee@gmail.com>
2020-10-03 10:14:45 -0600

    updated scenario1

--- scenarios.py
+++ scenarios.py
@@ -1,7 +1,7 @@
 scenario1 = {
     "id": 1,
     "title": "",
-    "description":
-    "suggestions": ["Tell me more about ___", "Have you considered ____", "We don't agree on ___ but we can agree on _____"]
+    "description": "A talkative new colleague is an avid tabloid and social media fan. She believes most of the things she reads a little too much, and talks constantly about refugees and asylum seekers being responsible for all kinds of issues that exist in our society. It has gotten to the point where you can't even talk about find a new property to rent without her loudly blaming immigrants for 'leeching off of the government' and 'getting handouts'.  Her husband has had a difficult time finding a job after being laid off, and she fully thinks immigrants are responsible and have 'taken all the jobs'. How do you respond to these statements?",
+    "suggestions": ["Have you considered ____","I don’t see it the way you do. I see it as __________.", "Tell me more about ___", "We don't agree on ___ but we can agree on _____"],
     "insights": ["How am I processing the experience?", "What body sensations do I have?", "What is my emotional reaction?"]
 }
