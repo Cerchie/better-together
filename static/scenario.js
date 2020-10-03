3684969df10e1b0f0001517e7fc84da605715901
Lucia Cerchie <luciacerchie@gmail.com>
2020-10-03 11:09:45 -0700

    working on js

--- static/scenario.js
+++ static/scenario.js
@@ -5,6 +5,7 @@ const form = document.querySelector('#form-user-response');
 const input = document.querySelector('#form-input');
 
 btn.addEventListener('click', function (e) {
+    console.log('click')
     e.preventDefault();
     answer.classList.remove('.hidden');
     responseDiv.innerText = input.value;
