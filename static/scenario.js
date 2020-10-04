0f211d404d8f2ecd485ef7b6d68a86cec4f92414
LSanchez17 <galaxyplayeremail@gmail.com>
2020-10-04 13:02:08 -0400

    fixed javascript display bug on scenario, minor scenario modification

--- static/scenario.js
+++ static/scenario.js
@@ -3,18 +3,14 @@ const answer = document.querySelector('#answer');
 const responseDiv = document.querySelector('.user-response');
 const form = document.querySelector('#form-user-response');
 const input = document.querySelector('#form-input');
+const usefulInfo = document.querySelector('#conversationStarters');
 
-btn.addEventListener('click', function (e) {
-    console.log('click');
-    e.preventDefault();
-    answer.classList.toggle('d-none');
-    responseDiv.innerText = input.value;
-    input.value = '';
-
-});
 
 form.addEventListener('submit', function (e) {
     console.log('click2');
     e.preventDefault();
-
+    answer.classList.toggle('d-none');    
+    usefulInfo.classList.toggle('d-none');
+    responseDiv.innerText = input.value;
+    input.value = '';
 });
