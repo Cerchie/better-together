f319b52a5a289872f000341b4bc8085d07d1f9ec
Jake <jakefrisbee@gmail.com>
2020-10-04 15:15:33 -0600

    added tests for make a scenario and ecuation resources

--- test_routes.py
+++ test_routes.py
@@ -43,18 +43,31 @@ class RoutesTestCase(TestCase):
             html = resp.get_data(as_text=True)
             self.assertIn('<p>Compare your response with the following:</p>', html)
 
-    def test_general_scenario_show(self):
-        """to see if scenarios show up"""
+    def test_create_scenario(self):
+        """to see if a user can creat their own scenario"""
         with app.test_client() as client: 
-            scenario = create_scenario(3434,'ice cream','so yummy', 'see the ice cream')
+            # scenario = create_scenario(3434,'ice cream','so yummy', 'see the ice cream')
 
-            resp = client.get(f"/scenario")
+            resp = client.post(f"/scenario_maker", data={"title": "TEST", 
+                                                        "description": "This is only a test", 
+                                                        "suggestions": "Suggestion 1", 
+                                                        "insights": "Insight 1"
+                                                        }, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("TEST", html)
+
+    def test_education(self):
+        """to see if resources page loads"""
+        with app.test_client() as client: 
+            resp = client.get('/education')
             self.assertEqual(resp.status_code, 200)
 
             html = resp.get_data(as_text=True)
-            self.assertIn('ice cream', html)
+            self.assertIn('Talking About', html)        
 
-####THIS TEST IS RETURNING THE JINJA TEMPLATE, SO THE HTML IS NOT FOUND
+
 
         
 
